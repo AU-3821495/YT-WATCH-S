4b9cdb0a2707e34f3b3b32eca6bd8991691d35a7
sennin361 <pokeopoke9@gmail.com>
2025-05-26 06:50:01 +0900

    Create service-worker.js
    仙人tube

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = "sennin-tube-v1";
+const assets = [
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
